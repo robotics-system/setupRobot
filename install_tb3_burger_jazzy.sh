03fbdb40634b0e255d4194d5127024b8b6f95b04
NiklasPerssonMDU <100279902+NiklasPerssonMDU@users.noreply.github.com>
2026-03-20 10:42:04 +0100

    Update install_tb3_burger_jazzy.sh

--- install_tb3_burger_jazzy.sh
+++ install_tb3_burger_jazzy.sh
@@ -2,43 +2,45 @@
 # Avbryt vid fel
 set -e
 
-echo "--- Startar automatiserad installation för TurtleBot3 Burger (Jazzy) ---"
+echo "--- STARTAR KOMPLETT INSTALLATION: TB3 BURGER (ROS 2 JAZZY) ---"
 
-# 1. Uppdatera listor och installera grundläggande verktyg
-sudo apt update
-sudo apt install -y software-properties-common curl wget
+# 1. Uppdatera och installera verktyg
+sudo apt update && sudo apt upgrade -y
+sudo apt install -y software-properties-common curl wget git
 
-# 2. FIX: Hantera "unmet dependencies" för bibliotek i Ubuntu 24.04
-# Vi tvingar installation av de versioner som ROS-paketen kräver
+# 2. FIX: Tvinga rätt versioner för Ubuntu 24.04 (Noble)
+# Detta löser "unmet dependencies" för lz4, zstd, zlib och bz2
 echo "--- Åtgärdar versionskonflikter för bibliotek ---"
 sudo apt install -y \
   liblz4-1=1.9.4-1build1 \
   libzstd1=1.5.5+dfsg2-2build1 \
   zlib1g=1:1.3.dfsg-3.1ubuntu2 \
+  libbz2-1.0=1.0.8-5.1 \
   --allow-downgrades
 
-# Lås dessa versioner tillfälligt för att undvika nya fel under ROS-install
-sudo apt-mark hold liblz4-1 libzstd1 zlib1g
+# Lås versionerna tillfälligt så att ROS-installationen inte kraschar
+sudo apt-mark hold liblz4-1 libzstd1 zlib1g libbz2-1.0
 
-# 3. Konfigurera ROS 2 Jazzy Repositories
-echo "--- Konfigurerar ROS 2 Jazzy Repository ---"
+# 3. Konfigurera ROS 2 Jazzy Repo
+echo "--- Lägger till ROS 2 Jazzy Repository ---"
 sudo add-apt-repository universe -y
 sudo curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.key -o /usr/share/keyrings/ros-archive-keyring.gpg
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] http://packages.ros.org/ros2/ubuntu $(. /etc/os-release && echo $UBUNTU_CODENAME) main" | sudo tee /etc/apt/sources.list.d/ros2.list > /dev/null
 
-# 4. Installera ROS 2 Base och utvecklarverktyg
+# 4. Installera ROS 2, TB3-paket, Bringup och Teleop
 sudo apt update
-sudo apt install -y ros-jazzy-ros-base python3-colcon-common-extensions
-
-# 5. Installera TurtleBot3-paket och drivrutiner
-echo "--- Installerar TurtleBot3-specifika paket ---"
+echo "--- Installerar ROS 2 Jazzy och TurtleBot3-paket ---"
 sudo apt install -y \
+  ros-jazzy-ros-base \
+  python3-colcon-common-extensions \
   ros-jazzy-turtlebot3-msgs \
   ros-jazzy-turtlebot3 \
+  ros-jazzy-turtlebot3-bringup \
+  ros-jazzy-turtlebot3-teleop \
   ros-jazzy-dynamixel-sdk \
   ros-jazzy-hls-lfcd-lds-driver
 
-# 6. Konfigurera OpenCR USB (udev-regler)
+# 5. Konfigurera OpenCR USB (udev-regler)
 echo "--- Konfigurerar USB-regler för OpenCR ---"
 if [ ! -f /etc/udev/rules.d/99-opencr-interface.rules ]; then
     wget https://raw.githubusercontent.com/ROBOTIS-GIT/OpenCR/master/99-opencr-interface.rules
@@ -48,14 +50,18 @@ if [ ! -f /etc/udev/rules.d/99-opencr-interface.rules ]; then
     rm 99-opencr-interface.rules
 fi
 
-# 7. Sätt miljövariabler i .bashrc (om de inte redan finns)
-echo "--- Uppdaterar .bashrc ---"
+# 6. Sätt miljövariabler i .bashrc
+echo "--- Uppdaterar miljövariabler i .bashrc ---"
 grep -qxF 'source /opt/ros/jazzy/setup.bash' ~/.bashrc || echo 'source /opt/ros/jazzy/setup.bash' >> ~/.bashrc
 grep -qxF 'export TURTLEBOT3_MODEL=burger' ~/.bashrc || echo 'export TURTLEBOT3_MODEL=burger' >> ~/.bashrc
 grep -qxF 'export LDS_MODEL=LDS-01' ~/.bashrc || echo 'export LDS_MODEL=LDS-01' >> ~/.bashrc
 
-# Släpp låset på biblioteken så systemet kan uppdateras normalt i framtiden
-sudo apt-mark unhold liblz4-1 libzstd1 zlib1g
+# 7. Släpp låset på biblioteken
+sudo apt-mark unhold liblz4-1 libzstd1 zlib1g libbz2-1.0
 
-echo "--- INSTALLATION KLAR ---"
-echo "Kör 'source ~/.bashrc' för att börja använda ROS 2 Jazzy."
+echo "-------------------------------------------------------"
+echo "INSTALLATION KLAR!"
+echo "1. Kör: source ~/.bashrc"
+echo "2. Flasha OpenCR (om ej gjort): ./flash_opencr_burger.sh"
+echo "3. Testa roboten: ./test_tb3.sh"
+echo "-------------------------------------------------------"
